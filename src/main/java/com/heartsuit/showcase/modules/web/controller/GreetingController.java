274df9d4335705ff830b89ed670cb914d66687b9
heartsuit <nxq0108@126.com>
2019-04-18 13:18:35 +0800

    add post in greeting controller

--- src/main/java/com/heartsuit/showcase/modules/web/controller/GreetingController.java
+++ src/main/java/com/heartsuit/showcase/modules/web/controller/GreetingController.java
@@ -1,6 +1,10 @@
 package com.heartsuit.showcase.modules.web.controller;
 
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +14,10 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting() {
         return "Hello DevCloud.";
+    }
+
+    @PostMapping("/greeting")
+    public String greeting(@RequestBody HashMap<String, String> params) {
+        return "Hello DevOps. Welcome " + params.get("name");
     }
 }
