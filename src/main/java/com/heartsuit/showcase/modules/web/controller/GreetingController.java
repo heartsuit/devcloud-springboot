4b22393b3ac7b2cca774503899059ff00377fe1e
heartsuit <nxq0108@126.com>
2019-04-18 13:18:13 +0800

    initial commit

---
+++ src/main/java/com/heartsuit/showcase/modules/web/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.heartsuit.showcase.modules.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello DevCloud.";
+    }
+}
